4dfd5bb09ed4b4865e20be0e96ce194427581021
Максим Смирнов <smirnoffmg@gmail.com>
2015-02-03 15:39:01 +0300

    fix #10

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
+# -*- coding: utf-8 -*-
 from distutils.core import setup
 
 setup(
     name='django-google-storage',
-    version='0.2.2dev',
+    version='0.4.0',
     packages=['django-google-storage', ],
     author='Maxim Smirnoff',
-    author_email='max@whitescape.com',
+    author_email='smirnoffmg@gmail.com',
     url='http://pypi.python.org/pypi/django-google-storage/',
     license='LICENSE.txt',
     description='Django storage for Google Storage',
-    long_description=open('README.txt').read(),
     install_requires=[
         "Django >= 1.4",
         "boto",
